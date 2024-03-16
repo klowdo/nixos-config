d91ce21096ecaa43fe149b005c856bd1639bf4ae
Felix Svensson <klowdo.fs@gmail.com>
2024-03-16 15:18:08 +0100

    add lazygit

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -58,7 +58,7 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  home.packages = with pkgs; [  ranger ];
+  home.packages = with pkgs; [  lazygit ranger ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
