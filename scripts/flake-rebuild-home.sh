a0bd42105735990c3594458543508ff633d7ac39
Felix Svensson <klowdo.fs@gmail.com>
2024-03-23 10:58:27 +0100

    add rebuild home sript

---
+++ scripts/flake-rebuild-home.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ ! -z $1 ]; then
+	export HOST=$1
+else
+	export HOST=$(hostname)
+fi
+
+# sudo nixos-rebuild --impure --flake .#$HOST switch
+# sudo nixos-rebuild switch --flake .#$HOST
+home-manager switch --flake .#klowdo@$HOST
