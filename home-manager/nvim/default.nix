d49463461a02e082322acb0264c72e9eae251521
Felix Svensson <klowdo.fs@gmail.com>
2024-03-23 10:57:56 +0100

    add python to nvim

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -2,25 +2,26 @@
 {
 
   programs.neovim = {
-  enable = true;
-   defaultEditor = true;
+    enable = true;
+    defaultEditor = true;
 
     viAlias = true;
-  vimAlias = true;
-};
-# to get lazyvim work with nix https://nixalted.com/
-# home.file = {
-# ".config/nvim".source = ./config;
-#
-# };
+    vimAlias = true;
+    withPython3 = true;
+  };
+  # to get lazyvim work with nix https://nixalted.com/
+  # home.file = {
+  # ".config/nvim".source = ./config;
+  #
+  # };
 
- home.packages = with pkgs; [
-      # CC
-      gcc
-      gnumake
-      cmake
-      autoconf
-      automake
-      libtool
+  home.packages = with pkgs; [
+    # CC
+    gcc
+    gnumake
+    cmake
+    autoconf
+    automake
+    libtool
   ];
 }
