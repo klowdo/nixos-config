86b2825e1f6b8cac94572243a361520534b8a7bf
Felix Svensson <klowdo.fs@gmail.com>
2024-09-12 21:18:22 +0200

    feat(hyprland): add volume and brightness binds

--- home-manager/desktops/hyprland3/binds.nix
+++ home-manager/desktops/hyprland3/binds.nix
@@ -75,6 +75,24 @@
       #editor = defaultApp "text/plain";
     in
       [
+        ################# Audio & Brightness ###################
+        ", xf86audioraisevolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", xf86audiolowervolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+
+        ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPrev, exec, playerctl previous"
+
+        # Keyboard backlight
+        ", keyboard_brightness_up_shortcut, exec, brightnessctl -d *::kbd_backlight set +33%"
+        ", keyboard_brightness_down_shortcut, exec, brightnessctl -d *::kbd_backlight set 33%-"
+
+        # Screen brightness
+        ", XF86MonBrightnessUp, exec, brightnessctl s +5%"
+        ", XF86MonBrightnessDown, exec, brightnessctl s 5%-"
+
         #################### Computer Mange ####################
 
         "${mainMod}, L, exec, ${hyprlock}"
