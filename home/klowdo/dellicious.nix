6285c7262e607e1f92d72798a8d7bbc9427bdca1
Felix Svensson <klowdo.fs@gmail.com>
2024-10-07 22:27:28 +0200

    fixes

--- home/klowdo/dellicious.nix
+++ home/klowdo/dellicious.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{config, ...}: {
   imports = [
     ./home.nix
     ./dotfiles
@@ -31,8 +31,16 @@
           sensitivity = -0.5;
         }
       ];
+
       monitor = [
-        "DP-1,1920x1080@60,0x0,1"
+        "eDP-1,3456x2160, 0x0, 1.5"
+        "DP-4 ,5120x2160@60,3456x0,1"
+      ];
+      # trigger when the switch is turning off
+      bindl = [
+        ", switch:off:Lid Switch,exec,hyprctl keyword monitor \"eDP-1,3456x2160, 0x0, 1.5\""
+        # trigger when the switch is turning on
+        ", switch:on:Lid Switch,exec,hyprctl keyword monitor \"eDP-1, disable\""
       ];
       workspace = [
         "1, monitor:DP-1, default:true"
@@ -46,4 +54,3 @@
     };
   };
 }
-
