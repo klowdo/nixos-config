1385e8503c501860c05535ad7bf5d0624e3cd1a8
Felix Svensson <klowdo.fs@gmail.com>
2024-10-17 17:08:10 +0200

    feat(dev/rider): add desktop file for rider to use wayland renderer

--- home/features/development/tools/rider.nix
+++ home/features/development/tools/rider.nix
@@ -7,11 +7,26 @@
 }:
 with lib; let
   cfg = config.features.development.tools.rider;
+  riderpkg = pkgs.unstable.jetbrains.rider;
 in {
   options.features.development.tools.rider.enable = mkEnableOption "enable rider IDE";
   config = mkIf cfg.enable {
     home.packages = [
-      pkgs.unstable.jetbrains.rider
+      riderpkg
     ];
+
+    home.file.".local/share/applications/jetbrains-rider.desktop".text = ''
+      [Desktop Entry]
+      Version=1.0
+      Type=Application
+      Name=JetBrains Rider
+      Icon=${riderpkg}/rider/bin/rider.svg
+      Exec="${riderpkg}/bin/rider" -Dawt.toolkit.name=WLToolkit %f
+      Comment=A cross-platform IDE for .NET
+      Categories=Development;IDE;
+      Terminal=false
+      StartupWMClass=jetbrains-rider
+      StartupNotify=true
+    '';
   };
 }
