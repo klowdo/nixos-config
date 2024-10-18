b720bfc00740be00bcd6269a2415290e6718f1d0
Felix Svensson <klowdo.fs@gmail.com>
2024-10-19 00:05:12 +0200

    feat(host/dell): disable nvidia

--- hosts/dellicious/configuration.nix
+++ hosts/dellicious/configuration.nix
@@ -10,6 +10,7 @@
     # waiting for https://github.com/NixOS/nixos-hardware/pull/881
     # should be xps 9530
     inputs.hardware.nixosModules.dell-xps-15-9520
+    inputs.hardware.nixosModules.common-gpu-nvidia-disable
     # inputs.hardware.nixosModules.common-cpu-intel
     # inputs.hardware.nixosModules.common-pc-laptop
     # inputs.hardware.nixosModules.common-pc-laptop-ssd
