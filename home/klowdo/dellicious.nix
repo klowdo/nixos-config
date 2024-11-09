1c7da56d438935507ff5881dfe670f6147c12631
Felix Svensson <klowdo.fs@gmail.com>
2024-11-09 23:47:52 +0100

    feat(wayland/hyprland): add way-displays

--- home/klowdo/dellicious.nix
+++ home/klowdo/dellicious.nix
@@ -31,9 +31,7 @@
     };
   };
 
-  wayland.windowManager.hyprland = let
-    laptopMonitor = "eDP-1,3456x2160@60,0x0,1.5";
-  in {
+  wayland.windowManager.hyprland = {
     settings = {
       input = {
         touchpad = {scroll_factor = 0.2;};
@@ -46,15 +44,6 @@
           name = "mouse";
           sensitivity = -0.5;
         }
-      ];
-      monitor = [
-        laptopMonitor
-        "DP-1,1920x1080@60,0x0,1"
-      ];
-      bindl = [
-        ", switch:off:Lid Switch,exec,hyprctl keyword monitor \"${laptopMonitor}\""
-        # trigger when the switch is turning on
-        ", switch:on:Lid Switch,exec,hyprctl keyword monitor \"eDP-1, disable\""
       ];
       workspace = [
         "1, monitor:eDP-1, default:true"
