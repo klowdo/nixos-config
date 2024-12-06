7415c99efb8e2b60132931973e36720c50e70194
Felix Svensson <klowdo.fs@gmail.com>
2024-12-06 23:51:53 +0100

    feat(git): more git settings

--- home/klowdo/home.nix
+++ home/klowdo/home.nix
@@ -80,6 +80,7 @@ in {
 
     pavucontrol
     zellij-ps
+    git-absorb
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -126,12 +127,30 @@ in {
   # program.firefox.enable = true;
   programs.git = {
     enable = true;
+    package = pkgs.gitAndTools.gitFull;
     userName = "Felix Svensson";
     userEmail = "klowdo.fs@gmail.com";
     aliases = {
       ci = "commit";
       co = "checkout";
       st = "status";
+    };
+    difftastic = {
+      enable = true;
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+
+      merge.conflictStyle = "zdiff3";
+      commit.verbose = true;
+      diff.algorithm = "histogram";
+      log.date = "iso";
+      column.ui = "auto";
+      branch.sort = "committerdate";
+      # Automatically track remote branch
+      push.autoSetupRemote = true;
+      # Reuse merge conflict fixes when rebasing
+      rerere.enabled = true;
     };
   };
 
