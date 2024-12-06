51adf4d606f023e5455a600fe3c7691d0caf7fa0
Felix Svensson <klowdo.fs@gmail.com>
2024-12-06 21:40:30 +0100

    chore(hyprland): fix keyboard layout

--- home/klowdo/dellicious.nix
+++ home/klowdo/dellicious.nix
@@ -34,11 +34,25 @@
   wayland.windowManager.hyprland = {
     settings = {
       input = {
-        touchpad = {scroll_factor = 0.2;};
+        touchpad = {
+          scroll_factor = 0.2;
+          disable_while_typing = true;
+        };
+        kb_layout = "us,se";
+        kb_options = "grp:win_space_toggle,ctrl:nocaps";
+        follow_mouse = 1;
+
+        sensitivity = 0;
       };
+
       device = [
         {
-          name = "keyboard";
+          name = "zsa-technology-labs-moonlander-mark-i-keyboard";
+          kb_layout = "us,se";
+        }
+        {
+          name = "zsa-technology-labs-moonlander-mark-i";
+          kb_layout = "us,se";
         }
         {
           name = "mouse";
