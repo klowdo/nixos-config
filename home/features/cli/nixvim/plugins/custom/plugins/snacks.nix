3d8d59c79664310b8e246c2c6173a1b1f435b4f0
Felix Svensson <klowdo.fs@gmail.com>
2024-12-17 07:45:33 +0100

    feat(nvim): add snacks to nvim

--- home/features/cli/nixvim/plugins/custom/plugins/snacks.nix
+++ home/features/cli/nixvim/plugins/custom/plugins/snacks.nix
@@ -3,6 +3,19 @@
   programs.nixvim = {
     plugins.snacks = {
       enable = true;
+      settings = {
+        bigfile = {enabled = true;};
+        dashboard = {enabled = true;};
+        indent = {enabled = true;};
+        input = {enabled = true;};
+        notifier = {enabled = true;};
+        quickfile = {enabled = true;};
+        scroll = {enabled = true;};
+        statuscolumn = {enabled = true;};
+        words = {enabled = true;};
+        terminal = {
+        };
+      };
     };
   };
 }
