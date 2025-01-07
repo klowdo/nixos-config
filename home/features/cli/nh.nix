827abaaf4a592623cddb9e566398d71fff4f915c
Felix Svensson <klowdo.fs@gmail.com>
2025-01-07 16:09:26 +0100

    chore(cli): move nh to use space

---
+++ home/features/cli/nh.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.features.cli.nh;
+in {
+  options.features.cli.nh.enable = mkEnableOption "enable nh cli tool";
+
+  config = mkIf cfg.enable {
+    programs.nh = {
+      enable = true;
+      flake = "/home/klowdo/.dotfiles/";
+      clean = {
+        enable = true;
+        extraArgs = "--keep-since 4d --keep 3";
+      };
+    };
+  };
+}
