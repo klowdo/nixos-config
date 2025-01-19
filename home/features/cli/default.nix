ec923ddf51d7a566f421f8de88caf722e2341c1d
Felix Svensson <klowdo.fs@gmail.com>
2025-01-19 17:47:10 +0100

    feat(cli): make zoxcide default cd

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -14,6 +14,9 @@
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
+    options = [
+      "--cmd cd" #replace cd with z and zi (via cdi)
+    ];
   };
 
   programs.eza = {
