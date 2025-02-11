2188826bb7d97c0828d6a9eb48eb6b1931a73ce6
Felix Svensson <klowdo.fs@gmail.com>
2025-02-11 17:12:14 +0100

    fixup! feat(audio): select inlcude dynamic inputs

--- home/klowdo/dotfiles/audio-select.sh
+++ home/klowdo/dotfiles/audio-select.sh
@@ -12,17 +12,21 @@ selected=$(
 	-p "Audio setup?" --hide-scroll --allow-markup  --width=650 --height=400
 )
 
+if [[ -z $selected ]]; then
+ exit 0
+fi
+
 if yq -e ".pairs[] | select(.name == \"$selected\")" $CONFIG_FILE >/dev/null 2>&1; then
 	mic=$(yq -e ".pairs[] | select(.name == \"$selected\")| .mic" -r $CONFIG_FILE)
 	selected=$(yq -e ".pairs[] | select(.name == \"$selected\")| .speaker" -r $CONFIG_FILE)
 
-	selectedMic=$(ponymix list-short --input | grep -i "$mic" |grep -i -v "mono" |grep -i -v "monitor" | awk -F '\t' '{print $2}')
+	selectedMic=$(ponymix list-short --input | grep -i "$mic" |grep -i -v "monitor" | awk -F '\t' '{print $2}')
 else 
 
 	mic=$(
 		ponymix list-short --sink | \
 		awk -F '\t' '{print $4}' | \
-		 wofi -i -M fuzzy -d  -p "Mic setup?" \
+		wofi -i -M fuzzy -d  -p "Mic setup?" \
 		 --hide-scroll --allow-markup  --width=650 --height=400
 	)
 
@@ -34,14 +38,15 @@ selectedSink=$(ponymix list-short --sink | grep -i "$selected" | awk -F '\t' '{p
 
 ponymix set-default --input -d "$selectedMic"
 ponymix set-default --sink -d "$selectedSink"
-#
 
-# echo "----MIC OUTPUT----"
-# ponymix list-short --input | grep "$selectedMic"
-#
-# echo "----SPEAKER OUTPUT----"
-#
-# echo "speaker for $selected"
-# ponymix list-short --sink | grep "$selectedSink"
-#
-# echo "----OUTPUT----"
+
+echo "----MIC OUTPUT----"
+echo "speaker for $selected"
+ponymix list-short --input | grep "$selectedMic"
+
+echo "----SPEAKER OUTPUT----"
+
+echo "speaker for $selected"
+ponymix list-short --sink | grep "$selectedSink"
+
+echo "----OUTPUT----"
