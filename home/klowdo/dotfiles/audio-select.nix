95c5e923197b3acb15189be77807f2232aaac0a9
Felix Svensson <klowdo.fs@gmail.com>
2025-02-12 08:38:02 +0100

    fixup! feat(audio): select inlcude dynamic inputs

--- home/klowdo/dotfiles/audio-select.nix
+++ home/klowdo/dotfiles/audio-select.nix
@@ -1,11 +1,10 @@
 {pkgs, ...}: let
   audio-select-script = pkgs.writeShellScriptBin "audio-select-script" ./audio-select.sh;
-  audio-select-config = ./audio-config.yml;
   audio-select = pkgs.writeShellApplication {
     name = "audio-select";
     runtimeInputs = with pkgs; [ponymix yq-go];
     text = ''
-      ${audio-select-script}/bin/audio-select-script ${audio-select-config}
+      ${audio-select-script}/bin/audio-select-script ${./audio-config.yml}
     '';
   };
 in {
