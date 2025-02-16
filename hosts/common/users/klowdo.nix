4c02428a1c851cf476561fc3c0bfa9578d2570e8
Felix Svensson <klowdo.fs@gmail.com>
2025-02-16 16:40:56 +0100

    chore: cleanup

--- hosts/common/users/klowdo.nix
+++ hosts/common/users/klowdo.nix
@@ -6,8 +6,10 @@
   ...
 }: {
   hardware.keyboard.zsa.enable = true;
+
   sops.secrets."passwords/klowdo".neededForUsers = true;
   users.mutableUsers = false;
+
   users.users.klowdo = {
     hashedPasswordFile = config.sops.secrets."passwords/klowdo".path;
     # password = lib.mkForce "nixos"; # Uncomment to set temporary password until sops passwords work
@@ -28,7 +30,8 @@
     openssh.authorizedKeys.keys = [
       (builtins.readFile ./klowdo/keys/id_ed25519.pub)
     ];
-    packages = [inputs.home-manager.packages.${pkgs.system}.default];
+    shell = pkgs.zsh;
+    packages = [pkgs.home-manager];
   };
   home-manager.users.klowdo =
     import ../../../home/klowdo/${config.networking.hostName}.nix;
