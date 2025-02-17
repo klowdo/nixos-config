cc7e6e5d9db8205cc9c66b7e3a8c2e013daa82e6
Felix Svensson <klowdo.fs@gmail.com>
2025-02-17 08:09:08 +0100

    feat(nixvim): move to own flake

--- home/features/cli/nixvim/plugins/custom/plugins/markview.nix
+++
@@ -1,8 +0,0 @@
-{
-  # https://nix-community.github.io/nixvim/plugins/markview/index.html
-  programs.nixvim = {
-    plugins.markview = {
-      enable = true;
-    };
-  };
-}
--- home/features/cli/nixvim/plugins/custom/plugins/tmux-navigator.nix
+++
@@ -1,8 +0,0 @@
-{
-  # https://nix-community.github.io/nixvim/plugins/tmux-navigator/index.html
-  programs.nixvim = {
-    plugins.tmux-navigator = {
-      enable = true;
-    };
-  };
-}
