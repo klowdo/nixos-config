7bd8eadb482a22004db1539fa2ffef4e569e8154
Felix Svensson <klowdo.fs@gmail.com>
2025-02-18 09:23:19 +0100

    fix(tmux): use correct colors for neovim

--- home/features/cli/tmux.nix
+++ home/features/cli/tmux.nix
@@ -81,9 +81,8 @@ in {
 
       extraConfig = ''
         # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-        set -g default-terminal "xterm-256color"
-        set -ga terminal-overrides ",*256col*:Tc"
-        set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+        set-option -g default-terminal "tmux-256color"
+        set-option -ga terminal-overrides ",xterm-256color:Tc"
         set-environment -g COLORTERM "truecolor"
 
         set-option -g status-position top
