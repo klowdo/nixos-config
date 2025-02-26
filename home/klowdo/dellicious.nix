3e24a1d5ad179305db40db8afdc153cfe6bb41d9
Felix Svensson <klowdo.fs@gmail.com>
2025-02-26 08:56:56 +0100

    feat(dellisious): add monitors

--- home/klowdo/dellicious.nix
+++ home/klowdo/dellicious.nix
@@ -94,31 +94,50 @@
     autoScaleMin = 1.0;
     scales = [
       {
+        # Internal
         nameDesc = "Samsung Display Corp. 0x414D";
         scale = 1.5;
       }
       {
+        # home TV
         nameDesc = "Samsung Electric Company SAMSUNG 0x01000E00";
         scale = 1.5;
+      }
+      {
+        # Work
+        nameDesc = "Samsung Electric Company LU28R55 H4ZT400162";
+        scale = 1.0;
+      }
+      {
+        # Home monitor
+        nameDesc = "HP Inc. HP Z40c G3 CN43090Z36";
+        scale = 1.0;
       }
     ];
     modes = [
       {
+        # Internal
         nameDesc = "Samsung Display Corp. 0x414D";
         width = 3456;
         height = 2160;
         hz = 60;
         max = true;
       }
-
       {
-        nameDesc = "Samsung Electric Company SAMSUNG 0x01000E00";
-        width = 3456;
-        height = 2160;
-        hz = 60;
+        # Work
+        nameDesc = "Samsung Electric Company LU28R55 H4ZT400162";
         max = true;
       }
       {
+        # Home Tv
+        nameDesc = "Samsung Electric Company SAMSUNG 0x01000E00";
+        width = 3840;
+        height = 2160;
+        # hz = 60;
+        max = true;
+      }
+      {
+        # Home monitor
         nameDesc = "HP Inc. HP Z40c G3 CN43090Z36";
         max = true;
       }
