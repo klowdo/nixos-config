2c2ff279ba885d678f1bee4e9960470c47c4e0c6
Felix Svensson <klowdo.fs@gmail.com>
2025-02-26 08:59:55 +0100

    feat(network/dns): add resolved

---
+++ hosts/common/optional/resolved.nix
@@ -0,0 +1,33 @@
+# https://github.com/mrkuz/nixos/blob/master/modules/nixos/resolved.nix
+{
+  config,
+  lib,
+  # pkgs,
+  # sources,
+  ...
+}:
+with lib; let
+  cfg = config.modules.resolved;
+in {
+  options.modules.resolved = {
+    enable = mkOption {
+      default = false;
+      type = types.bool;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.resolved = {
+      enable = true;
+      # extraConfig = mkIf config.modules.avahi.enable "MulticastDNS=false";
+    };
+
+    # networking.networkmanager.extraConfig = "[connection]\nconnection.mdns=2\n";
+
+    # environment.etc.openvpn.source = "${pkgs.update-systemd-resolved}/libexec/openvpn";
+    #
+    # environment.systemPackages = with pkgs; [
+    #   update-systemd-resolved
+    # ];
+  };
+}
