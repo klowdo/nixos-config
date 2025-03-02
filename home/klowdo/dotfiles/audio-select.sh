fa5540f19563ed3a1548985bb01e856928f6b918
Felix Svensson <klowdo.fs@gmail.com>
2025-03-02 17:21:39 +0100

    feat(audio): remove profiles

--- home/klowdo/dotfiles/audio-select.sh
+++ home/klowdo/dotfiles/audio-select.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-CONFIG_FILE=${1:-./audio-config.yml}
+# CONFIG_FILE=${1:-./audio-config.yml}
 
 available="$(ponymix list-short --sink | awk -F '\t' '{print $4}')"
 
-known="$(yq '.pairs[].name' -r "$CONFIG_FILE")"
+# known="$(yq '.pairs[].name' -r "$CONFIG_FILE")"
 
 selected=$(
-	printf '%s\n%s\n' "$known" "$available" |
+	printf '%s' "$available" |
 		wofi -i -M fuzzy -d \
 			-p "Audio setup?" --hide-scroll --allow-markup --width=650 --height=400
 )
@@ -16,23 +16,23 @@ if [[ -z $selected ]]; then
 	exit 0
 fi
 
-if yq -e ".pairs[] | select(.name == \"$selected\")" "$CONFIG_FILE" >/dev/null 2>&1; then
-	mic=$(yq -e ".pairs[] | select(.name == \"$selected\")| .mic" -r "$CONFIG_FILE")
-	selected=$(yq -e ".pairs[] | select(.name == \"$selected\")| .speaker" -r "$CONFIG_FILE")
+# if yq -e ".pairs[] | select(.name == \"$selected\")" "$CONFIG_FILE" >/dev/null 2>&1; then
+# 	mic=$(yq -e ".pairs[] | select(.name == \"$selected\")| .mic" -r "$CONFIG_FILE")
+# 	selected=$(yq -e ".pairs[] | select(.name == \"$selected\")| .speaker" -r "$CONFIG_FILE")
+#
+# 	selectedMic=$(ponymix list-short --input | grep -i "$mic" | grep -i -v "monitor" | awk -F '\t' '{print $2}')
+# else
 
-	selectedMic=$(ponymix list-short --input | grep -i "$mic" | grep -i -v "monitor" | awk -F '\t' '{print $2}')
-else
+mic=$(
+	ponymix list-short --sink |
+		awk -F '\t' '{print $4}' |
+		wofi -i -M fuzzy -d -p "Mic setup?" \
+			--hide-scroll --allow-markup --width=650 --height=400
+)
 
-	mic=$(
-		ponymix list-short --sink |
-			awk -F '\t' '{print $4}' |
-			wofi -i -M fuzzy -d -p "Mic setup?" \
-				--hide-scroll --allow-markup --width=650 --height=400
-	)
+selectedMic=$(ponymix list-short --input | grep -i "$mic" | awk -F '\t' '{print $2}')
 
-	selectedMic=$(ponymix list-short --input | grep -i "$mic" | awk -F '\t' '{print $2}')
-
-fi
+# fi
 
 selectedSink=$(ponymix list-short --sink | grep -i "$selected" | awk -F '\t' '{print $2}')
 
