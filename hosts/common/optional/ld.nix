b892845384b17e41b0227a1ffe294ea3d3fb320b
Felix Svensson <klowdo.fs@gmail.com>
2025-03-03 18:16:50 +0100

    feat(ld): add icu

--- hosts/common/optional/ld.nix
+++ hosts/common/optional/ld.nix
@@ -3,5 +3,10 @@
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
     mpifileutils
+    icu # for aspire 9.1 localization
+  ];
+
+  environment.systemPackages = with pkgs; [
+    icu # for aspire 9.1 localization
   ];
 }
