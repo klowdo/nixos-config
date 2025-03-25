0363e0068a2db2ff18db383cd33f400267d4d9a6
Felix Svensson <klowdo.fs@gmail.com>
2025-03-25 16:03:56 +0100

    chore(dellicious): add icu dev

--- hosts/dellicious/default.nix
+++ hosts/dellicious/default.nix
@@ -43,6 +43,7 @@
     ../common/optional/default.nix
     ../common/optional/tailscale-exit-node.nix
     ../common/optional/udev.nix
+    ../common/optional/icu.nix
   ];
 
   powerManagement.enable = true;
