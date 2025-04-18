0e844378f3cf97b84ddc513c5188e423e7ad63b3
Felix Svensson <klowdo.fs@gmail.com>
2025-04-18 08:12:52 +0200

    fix(strongwan): update script to handle if interfave is not present

--- hosts/common/optional/services/worldstream/strongSwan.nix
+++ hosts/common/optional/services/worldstream/strongSwan.nix
@@ -12,6 +12,12 @@ with lib; let
     pkgs.writeShellScriptBin "ws-vpn"
     # shell
     ''
+      INTERFACES=("enp0s20f0u1u1" "wlp0s20f3")
+      # HOMEDNS=192.168.1.252
+      HOMEDNS=${homedns}
+      DNS1=10.10.16.10
+      DNS2=10.10.17.10
+
       if [ "$1" == 'up' ]; then
         tailscale_status=$(${pkgs.tailscale}/bin/tailscale status  --json | ${pkgs.jq}/bin/jq .BackendState -r)
         if [ "$tailscale_status" == 'Running' ]; then
@@ -19,14 +25,15 @@ with lib; let
         fi
         ${pkgs.strongswan}/bin/ipsec up ${connecionName}
 
-        INTERFACES=("enp0s20f0u1u1" "wlp0s20f3")
-        # HOMEDNS=192.168.1.252
-        HOMEDNS=${homedns}
-        DNS1=10.10.16.10
-        DNS2=10.10.17.10
 
         for INT in "''${INTERFACES[@]}"; do
-          sudo resolvectl dns "$INT" "$DNS1" "$DNS2"
+          # Check if interface exists
+          if ip link show "$INT" &>/dev/null; then
+            echo "Setting DNS for interface $INT"
+            sudo resolvectl dns "$INT" "$DNS1" "$DNS2"
+          else
+            echo "Interface $INT is not available, skipping"
+          fi
         done
 
 
@@ -34,8 +41,16 @@ with lib; let
         ${pkgs.strongswan}/bin/ipsec down ${connecionName}
 
         for INT in "''${INTERFACES[@]}"; do
-          sudo resolvectl dns "$INT" "$HOMEDNS"
+          # Check if interface exists
+          if ip link show "$INT" &>/dev/null; then
+            echo "Setting HOME DNS for interface $INT"
+            sudo resolvectl dns "$INT" "$HOMEDNS"
+          else
+            echo "Interface $INT is not available, skipping"
+          fi
         done
+
+
         ${pkgs.tailscale}/bin/tailscale up
       else
         echo "have no idea what to do"
