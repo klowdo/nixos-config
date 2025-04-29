55debcbd33c5a1cb6583ebffa1c9404573af072f
Felix Svensson <klowdo.fs@gmail.com>
2025-04-29 15:01:11 +0200

    fix(docker): add client dns

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -12,21 +12,37 @@ in {
   config = mkIf cfg.enable {
     # users.extraGroups.docker.members = ["username-with-access-to-socket"];
     # users.users.klowdo.extraGroups = ["docker"];
-
+    networking.firewall.checkReversePath = "loose";
     virtualisation = {
       docker = {
-        enable = true;
+        # enable = true;
+        # extraOptions = "--bip=192.168.100.1/24";
         rootless = {
+          # extraOptions = "--bip=192.168.100.1/24";
           enable = true;
           setSocketVariable = true;
-
+          #
           daemon.settings = {
-            dns = ["8.8.8.8" "1.1.1.1"];
+            dns = [
+              "8.8.8.8"
+              "1.1.1.1"
+              "10.244.0.15"
+              "10.244.0.20"
+            ];
+            # bip = "192.168.100.1/24";
+            # fixed-cidr": "192.168.1.0/25",
+            # "mtu": 1500,
+            # network-mode = "vpnkit"; # default-gateway = "192.168.100.254";
           };
         };
-        daemon.settings = {
-          dns = ["8.8.8.8" "1.1.1.1"];
-        };
+        # daemon.settings = {
+        #   dns = ["8.8.8.8" "1.1.1.1"];
+        #   # # fixed-cidr = "182.168.1.0/25";
+        #   bip = "172.168.2.1/24";
+        #   # # fixed-cidr": "192.168.1.0/25",
+        #   # # "mtu": 1500,
+        #   # default-gateway = "172.168.2.254";
+        # };
         autoPrune = {
           enable = true;
           dates = "daily";
@@ -39,9 +55,15 @@ in {
       };
     };
 
+    boot.kernel.sysctl = {
+      "kernel.unprivileged_userns_clone" = "1";
+    };
+
     environment.systemPackages = with pkgs; [
       unstable.lazydocker
       unstable.dive
+      slirp4netns
+      fuse-overlayfs
     ];
   };
 }
