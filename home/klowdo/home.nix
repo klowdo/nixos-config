5137c982f70b65c04dc2004fa3495796ba436542
Felix Svensson <klowdo.fs@gmail.com>
2025-05-04 21:20:51 +0200

    feat(home): add vesktop

--- home/klowdo/home.nix
+++ home/klowdo/home.nix
@@ -29,6 +29,7 @@ in {
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    vesktop
     fastfetch
     gsettings-desktop-schemas
     swaynotificationcenter
