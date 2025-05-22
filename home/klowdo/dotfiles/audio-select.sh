18a6af15131e1c14e3e320159e4855af38b9ec92
Felix Svensson <klowdo.fs@gmail.com>
2025-05-22 10:09:25 +0200

    feat(audio): fix audio select

--- home/klowdo/dotfiles/audio-select.sh
+++ home/klowdo/dotfiles/audio-select.sh
@@ -1,51 +1,37 @@
 #!/usr/bin/env bash
 
-# CONFIG_FILE=${1:-./audio-config.yml}
-
-available="$(ponymix list-short --sink | awk -F '\t' '{print $4}')"
-
-# known="$(yq '.pairs[].name' -r "$CONFIG_FILE")"
-
-selected=$(
-	printf '%s' "$available" |
+selectedSink=$(
+	ponymix list-short --sink |
+		awk -F '\t' '{print $4}' |
 		wofi -i -M fuzzy -d \
 			-p "Audio setup?" --hide-scroll --allow-markup --width=650 --height=400
 )
 
-if [[ -z $selected ]]; then
-	exit 0
+if [[ -n $selectedSink ]]; then
+
+	selectedSinkID=$(ponymix list-short --sink | awk -F '\t' -v desc="$selectedSink" '$4 == desc {print $2}')
+	ponymix set-default --output -d "$selectedSinkID"
 fi
 
-# if yq -e ".pairs[] | select(.name == \"$selected\")" "$CONFIG_FILE" >/dev/null 2>&1; then
-# 	mic=$(yq -e ".pairs[] | select(.name == \"$selected\")| .mic" -r "$CONFIG_FILE")
-# 	selected=$(yq -e ".pairs[] | select(.name == \"$selected\")| .speaker" -r "$CONFIG_FILE")
-#
-# 	selectedMic=$(ponymix list-short --input | grep -i "$mic" | grep -i -v "monitor" | awk -F '\t' '{print $2}')
-# else
-
 mic=$(
-	ponymix list-short --sink |
+	ponymix list-short --input |
 		awk -F '\t' '{print $4}' |
 		wofi -i -M fuzzy -d -p "Mic setup?" \
 			--hide-scroll --allow-markup --width=650 --height=400
 )
 
-selectedMic=$(ponymix list-short --input | grep -i "$mic" | awk -F '\t' '{print $2}')
-
-# fi
-
-selectedSink=$(ponymix list-short --sink | grep -i "$selected" | awk -F '\t' '{print $2}')
-
-ponymix set-default --input -d "$selectedMic"
-ponymix set-default --sink -d "$selectedSink"
+if [[ -n $mic ]]; then
+	selectedMicID=$(ponymix list-short --input | awk -F '\t' -v desc="$mic" '$4 == desc {print $2}')
+	ponymix set-default --input -d "$selectedMicID"
+fi
 
 echo "----MIC OUTPUT----"
-echo "speaker for $selected"
-ponymix list-short --input | grep "$selectedMic"
+echo "mic for $mic"
+ponymix list-short --input | grep "$selectedMicID"
 
 echo "----SPEAKER OUTPUT----"
 
-echo "speaker for $selected"
-ponymix list-short --sink | grep "$selectedSink"
+echo "speaker for $selectedSink"
+ponymix list-short --sink | grep "$selectedSinkID"
 
 echo "----OUTPUT----"
