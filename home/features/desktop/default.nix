58958d241916ed4e81e1ef84745ae88a8f0eba25
Felix Svensson <klowdo.fs@gmail.com>
2025-08-25 07:33:59 +0200

    feat(desktop): add tailscale systray

--- home/features/desktop/default.nix
+++ home/features/desktop/default.nix
@@ -1,12 +1,10 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./fonts.nix
     ./wayland.nix
     ./hyprland.nix
     ./notification.nix
     ./clipboard.nix
-  ];
-
-  home.packages = with pkgs; [
+    ./tailscale-systray.nix
   ];
 }
