388f8d238ceef6e58fdbf0ee2cfbae4eb21fed6f
Felix Svensson <klowdo.fs@gmail.com>
2025-10-07 15:07:17 +0200

    feat(printing): add cups drivers

--- hosts/common/optional/printing.nix
+++ hosts/common/optional/printing.nix
@@ -1,4 +1,11 @@
-{
+{pkgs, ...}: {
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+    ];
+  };
 }
