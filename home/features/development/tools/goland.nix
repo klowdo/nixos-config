44c9100d165a7107fc5eb5751aa3fe5f051768e9
Felix Svensson <klowdo.fs@gmail.com>
2025-11-12 09:32:53 +0100

    chore(goland): update

--- home/features/development/tools/goland.nix
+++ home/features/development/tools/goland.nix
@@ -7,15 +7,26 @@
 with lib; let
   cfg = config.features.development.tools.goland;
   inherit (pkgs.unstable.jetbrains) plugins;
-  golandPkg = pkgs.unstable.jetbrains.goland;
+# https://www.jetbrains.com/go/download/other.html
+  golandPkg = pkgs.unstable.jetbrains.goland.overrideAttrs
+    (_old: {
+      src = pkgs.fetchurl {
+        url = "https://download.jetbrains.com/go/goland-2025.2.4.tar.gz";
+        hash = "sha256-+0KXjVUnHm+jFlsdAQs+C6y4zVxPQHNxIzLGLzLS66s=";
+      };
+      # https://www.jetbrains.com/rider/download/other.html
+      version = "2025.2.4";
+      build_number = "252.26830.102";
+
+    });
   # Extract version from GoLand package (e.g., "2025.1.2" -> "2025.1")
   golandVersion = builtins.concatStringsSep "." (take 2 (splitString "." golandPkg.version));
 in {
   options.features.development.tools.goland.enable = mkEnableOption "enable goland IDE";
   config = mkIf cfg.enable {
     home.packages = [
-      (plugins.addPlugins pkgs.unstable.jetbrains.goland [
-        "github-copilot"
+      (plugins.addPlugins golandPkg [
+        # "github-copilot"
         "ideavim"
       ])
     ];
