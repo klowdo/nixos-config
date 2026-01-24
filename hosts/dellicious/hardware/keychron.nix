12af3b1d77e67544d72e6175ea7e270c79ce0c09
Felix Svensson <klowdo.fs@gmail.com>
2026-01-24 22:00:23 +0100

    fix(hardware/keyboard): use nix os keychron module

--- hosts/dellicious/hardware/keychron.nix
+++ hosts/dellicious/hardware/keychron.nix
@@ -1,13 +1,6 @@
-{  pkgs, ... }:
-
-let
-  keychron-set-key = pkgs.writeShellScriptBin "keychron-set-key" ''
-    echo 0 > /sys/module/hid_apple/parameters/fnmode
-  '';
-in {
-  environment.systemPackages = [ keychron-set-key ];
-
-  boot.initrd.services.udev.rules = ''
-  ACTION=="add", ATTRS{idVendor}=="05ac", ATTRS{idProduct}=="0250", RUN+="/run/current-system/sw/bin/keychron-set-key"
-  '';
+{
+  hardware.keyboard.qmk = {
+    enable = true;
+    keychronSupport = true;
+  };
 }
