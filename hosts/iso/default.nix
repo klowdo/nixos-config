94402ff9c0311659f854e00d632a0d1ce45364e4
Felix Svensson <klwodo.fs@gmail.com>
2026-01-27 16:13:56 +0000

    fix(iso): use modulesPath for installer imports
    - Use modulesPath instead of inputs.nixpkgs for installer module paths
    - Remove disko default.nix to avoid import scope issues

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -4,11 +4,12 @@
   pkgs,
   lib,
   inputs,
+  modulesPath,
   ...
 }: {
   imports = [
-    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    "${modulesPath}/installer/cd-dvd/channel.nix"
     inputs.disko.nixosModules.disko
   ];
 
