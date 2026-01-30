fcf88d36a6f012ed5d70c185009046ecf81302d9
Felix Svensson <klowdo.fs@gmail.com>
2026-01-30 13:40:23 +0100

    chore(vicinae): style handled by stylix

--- home/features/desktop/vicinae.nix
+++ home/features/desktop/vicinae.nix
@@ -79,43 +79,8 @@ in {
         };
         window = {
           csd = true;
-          opacity = 0.95;
+          # opacity = 0.95;
           rounding = 10;
-        };
-        theme = {
-          name = "catppuccin-mocha";
-        };
-      };
-      themes = {
-        catppuccin-mocha = {
-          meta = {
-            version = 1;
-            name = "Catppuccin Mocha";
-            description = "Cozy feeling with color-rich accents";
-            variant = "dark";
-            icon = "icons/catppuccin-mocha.png";
-            inherits = "vicinae-dark";
-          };
-
-          colors = {
-            core = {
-              background = "#1E1E2E";
-              foreground = "#CDD6F4";
-              secondary_background = "#181825";
-              border = "#313244";
-              accent = "#89B4FA";
-            };
-            accents = {
-              blue = "#89B4FA";
-              green = "#A6E3A1";
-              magenta = "#F5C2E7";
-              orange = "#FAB387";
-              purple = "#CBA6F7";
-              red = "#F38BA8";
-              yellow = "#F9E2AF";
-              cyan = "#94E2D5";
-            };
-          };
         };
       };
     };
