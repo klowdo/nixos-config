3bc9ed97130073a8d060e1889ac9f688ffae1ba6
Felix Svensson <klowdo.fs@gmail.com>
2026-01-30 14:33:27 +0100

    chore(zsh): fix deprecation warning

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -45,29 +45,30 @@ in {
         }
       ];
 
-      initExtraFirst = ''
-        # Add just command runner completions to fpath (before compinit)
-        fpath=(${pkgs.just}/share/zsh/site-functions $fpath)
-      '';
-
-      initContent = ''
-        function sesh-sessions() {
-          {
-            exec </dev/tty
-            exec <&1
-            local session
-            session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
-            zle reset-prompt > /dev/null 2>&1 || true
-            [[ -z "$session" ]] && return
-            sesh connect $session
+      initContent = lib.mkMerge [
+        (lib.mkBefore ''
+          # Add just command runner completions to fpath (before compinit)
+          fpath=(${pkgs.just}/share/zsh/site-functions $fpath)
+        '')
+        ''
+          function sesh-sessions() {
+            {
+              exec </dev/tty
+              exec <&1
+              local session
+              session=$(sesh list -t -c | fzf --height 40% --reverse --border-label ' sesh ' --border --prompt '⚡  ')
+              zle reset-prompt > /dev/null 2>&1 || true
+              [[ -z "$session" ]] && return
+              sesh connect $session
+            }
           }
-        }
 
-        zle     -N             sesh-sessions
-        bindkey -M emacs '\es' sesh-sessions
-        bindkey -M vicmd '\es' sesh-sessions
-        bindkey -M viins '\es' sesh-sessions
-      '';
+          zle     -N             sesh-sessions
+          bindkey -M emacs '\es' sesh-sessions
+          bindkey -M vicmd '\es' sesh-sessions
+          bindkey -M viins '\es' sesh-sessions
+        ''
+      ];
 
       loginExtra = ''
           export NIX_LOG=info
