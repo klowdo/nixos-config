01d805370c43858f593a63ee67c371eaf05a12eb
Felix Svensson <klowdo.fs@gmail.com>
2026-02-01 21:41:55 +0100

    feat(auto-upgrade): add notification

--- hosts/common/optional/auto-upgrade.nix
+++ hosts/common/optional/auto-upgrade.nix
@@ -14,6 +14,21 @@
     if cfg.dotfilesPath != ""
     then cfg.dotfilesPath
     else "${effectiveHome}/.dotfiles";
+  notifyScript = pkgs.writeShellScript "notify-upgrade" ''
+    STATUS="$1"
+    MESSAGE="$2"
+    URGENCY="''${3:-normal}"
+
+    export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u ${cfg.mainUser})/bus"
+    ${pkgs.sudo}/bin/sudo -u ${cfg.mainUser} \
+      DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" \
+      ${pkgs.libnotify}/bin/notify-send \
+        -u "$URGENCY" \
+        -a "NixOS Upgrade" \
+        -i "system-software-update" \
+        "NixOS Auto-Upgrade: $STATUS" \
+        "$MESSAGE"
+  '';
 in {
   system.autoUpgrade = {
     enable = true;
@@ -38,8 +53,23 @@ in {
 
   # Pull latest config from GitHub before auto-upgrade runs
   systemd.services.nixos-upgrade = {
+    unitConfig = {
+      OnFailure = ["notify-upgrade-failure.service"];
+    };
     serviceConfig = {
-      ExecStartPre = "${pkgs.git}/bin/git -C ${configDir} pull --ff-only origin main";
+      ExecStartPre = [
+        "${pkgs.git}/bin/git -C ${configDir} pull --ff-only origin main"
+        "${notifyScript} Started 'Pulling latest config and upgrading system...'"
+      ];
+      ExecStartPost = "${notifyScript} Completed 'System upgrade finished successfully'";
+    };
+  };
+
+  systemd.services.notify-upgrade-failure = {
+    description = "Notify on NixOS upgrade failure";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${notifyScript} Failed 'System upgrade failed! Check journalctl -u nixos-upgrade' critical";
     };
   };
 }
