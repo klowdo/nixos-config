133250d9ce3db43fb5c6e34852be91d82f2ecf05
Felix Svensson <klowdo.fs@gmail.com>
2026-02-01 19:57:50 +0100

    chore(steam-link): add game mode

--- home/features/gaming/steam-link.nix
+++ home/features/gaming/steam-link.nix
@@ -1,10 +1,12 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib; let
   cfg = config.features.gaming.steam-link;
+  notify = msg: "${pkgs.libnotify}/bin/notify-send -u low -t 2000 'Game Mode' '${msg}'";
 in {
   options.features.gaming.steam-link.enable = mkEnableOption "Steam Link client for game streaming";
 
@@ -24,14 +26,18 @@ in {
       ];
 
       bind = [
+        "SUPER, F12, exec, ${notify "Enabled - keys pass through"}"
         "SUPER, F12, submap, gamemode"
       ];
     };
 
     wayland.windowManager.hyprland.extraConfig = ''
       submap = gamemode
+      bind = SUPER, F12, exec, ${notify "Disabled"}
       bind = SUPER, F12, submap, reset
+      bind = SUPER SHIFT, F12, exec, ${notify "Disabled"}
       bind = SUPER SHIFT, F12, submap, reset
+      bind = , escape, exec, ${notify "Disabled"}
       bind = , escape, submap, reset
       submap = reset
     '';
