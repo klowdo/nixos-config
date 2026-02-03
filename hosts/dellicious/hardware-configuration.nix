ca13259e523147375caac9d0a065e77282c18044
Felix Svensson <klowdo.fs@gmail.com>
2026-02-03 09:37:45 +0100

    chore(dellicious): update hardware disk labels

--- hosts/dellicious/hardware-configuration.nix
+++ hosts/dellicious/hardware-configuration.nix
@@ -17,24 +17,17 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/2863d446-b8b1-41a6-b780-5a32d4013a7d";
+    device = "/dev/disk/by-uuid/b8e67ffd-5876-4c08-9000-77ac86957e54";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/3CCC-E979";
     fsType = "vfat";
+    options = ["fmask=0022" "dmask=0022"];
   };
 
   swapDevices = [];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s13f0u1u4.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
