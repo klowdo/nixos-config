7c7284c5a1eff2466d3911ef0125acbca72512b8
Felix Svensson <klowdo.fs@gmail.com>
2026-02-07 22:52:26 +0100

    feat(tailscale): use sops

--- hosts/common/core/tailscale.nix
+++ hosts/common/core/tailscale.nix
@@ -1,7 +1,19 @@
-{lib, ...}: {
+{
+  lib,
+  config,
+  ...
+}: {
+  sops.secrets."applications/tailscale/authkey" = {};
+
   services.tailscale = {
     enable = true;
     useRoutingFeatures = lib.mkDefault "client";
+    authKeyFile = config.sops.secrets."applications/tailscale/authkey".path;
+    extraUpFlags = [
+      "--accept-routes"
+      "--accept-dns"
+      "--ssh"
+    ];
   };
   networking.firewall.allowedUDPPorts = [41641]; # Facilitate firewall punching
 }
