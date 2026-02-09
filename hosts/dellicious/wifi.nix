873cdab515189d36afa68b90f0531a58e96553fb
Felix Svensson <klowdo.fs@gmail.com>
2026-02-09 17:51:01 +0100

    feat(wifi): add phone password

--- hosts/dellicious/wifi.nix
+++ hosts/dellicious/wifi.nix
@@ -8,6 +8,11 @@
         ssid = "Napolitana Felix";
         sopsKey = "wifi/home-felix";
       };
+
+      "phone" = {
+        ssid = "Felix green phone";
+        sopsKey = "wifi/phone-felix";
+      };
       "work" = {
         ssid = "Pelagia";
         sopsKey = "wifi/work-pelagia";
