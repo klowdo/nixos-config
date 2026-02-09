bcbbf28bc3b398df435883eac7d7a3df166929c1
Felix Svensson <klowdo.fs@gmail.com>
2026-02-09 13:21:14 +0100

    feat(ssh): add pre known hosts and secret config

--- home/features/cli/ssh.nix
+++ home/features/cli/ssh.nix
@@ -9,40 +9,27 @@ with lib; let
 in {
   options.features.cli.ssh.enable = mkEnableOption "SSH client with GNOME Keyring integration";
 
-  config =
-    mkIf cfg.enable {
-      programs.ssh = {
-        enable = true;
-        enableDefaultConfig = false;
-        includes = [
-          "config.d/*"
-        ];
-
-        matchBlocks = {
-          "*" = {
-            addKeysToAgent = "yes";
-            forwardAgent = true;
-            identityAgent = "\${XDG_RUNTIME_DIR}/keyring/ssh";
-          };
-          "github.com" = {
-            user = "git";
-            identityFile = "~/.ssh/id_ed25519
-";
-          };
-        };
-        extraConfig = ''
-          # Use libsecret for password authentication when needed
-          # This allows SSH to integrate with GNOME Keyring for passwords
+  config = mkIf cfg.enable {
+    sops.secrets = {
+      "ssh/customer-1/config" = {
+        path = "${config.home.homeDirectory}/.ssh/config.d/customer-1.conf";
+        mode = "0440";
+      };
+      "ssh/customer-1/hostkey" = {
+        path = "${config.home.homeDirectory}/.ssh/known_hosts.d/customer-1";
+        mode = "0440";
+      };
+    };
+    home.file = {
+      ".ssh/known_hosts.d/public" = {
+        text = ''
+          github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+          github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
         '';
       };
 
-      # Add libsecret tools for password management
-      home.packages = with pkgs; [
-        libsecret # Provides secret-tool command
-      ];
-
       # Helper script for SSH with keyring password lookup
-      home.file.".local/bin/ssh-keyring" = {
+      ".local/bin/ssh-keyring" = {
         text = ''
           #!/usr/bin/env bash
           # SSH wrapper that can retrieve passwords from GNOME Keyring
@@ -71,4 +58,58 @@ in {
         executable = true;
       };
     };
+    # sops.templates = {
+    #   # "ssh-customer-1-config" = {
+    #   #   content = config.sops.placeholder."ssh/customer-1/config";
+    #   #   path = "${config.home.homeDirectory}/.ssh/config.d/customer-1.conf";
+    #   #   mode = "0600";
+    #   # };
+    #
+    #   "ssh-known-hosts-declarative" = {
+    #     content = ''
+    #       # GitHub
+    #       github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+    #       github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+    #
+    #       # Customer hosts (from SOPS)
+    #       ${config.sops.placeholder."ssh/customer-1/hostkey"}
+    #     '';
+    #     path = "${config.home.homeDirectory}/.ssh/known_hosts.d/declarative";
+    #     mode = "0600";
+    #   };
+    # };
+
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      includes = [
+        "config.d/*"
+      ];
+
+      matchBlocks = {
+        "*" = {
+          addKeysToAgent = "yes";
+          forwardAgent = true;
+          identityAgent = "\${XDG_RUNTIME_DIR}/keyring/ssh";
+          extraOptions = {
+            UserKnownHostsFile = "~/.ssh/known_hosts";
+            KnownHostsCommand = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/cat ~/.ssh/known_hosts.d/* 2>/dev/null || true'";
+          };
+        };
+        "github.com" = {
+          user = "git";
+          identityFile = "~/.ssh/id_ed25519";
+        };
+      };
+      extraConfig = ''
+        # Use libsecret for password authentication when needed
+        # This allows SSH to integrate with GNOME Keyring for passwords
+      '';
+    };
+
+    # Add libsecret tools for password management
+    home.packages = with pkgs; [
+      libsecret # Provides secret-tool command
+    ];
+  };
 }
