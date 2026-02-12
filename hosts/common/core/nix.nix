cec2b5fc6197a9767f1059857899feaaccebee02
Felix Svensson <klowdo.fs@gmail.com>
2026-02-12 16:01:05 +0100

    fix(nix): trust cachix substituter to accept signed builds

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -5,6 +5,9 @@
     extra-substituters = [
       "https://klowdo.cachix.org"
     ];
+    extra-trusted-substituters = [
+      "https://klowdo.cachix.org"
+    ];
     extra-trusted-public-keys = [
       "klowdo.cachix.org-1:FY4Cq5DPh6QYyU2R2PGgCNdXbh7u6H3kRRpmydZbdXc="
     ];
