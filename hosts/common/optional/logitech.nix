7a18aa9c705d748658f043db4e8a0b69477918a3
Felix Svensson <klowdo.fs@gmail.com>
2026-02-26 08:16:28 +0100

    chore(solaar): only window

--- hosts/common/optional/logitech.nix
+++ hosts/common/optional/logitech.nix
@@ -7,10 +7,11 @@
   systemd.user.services.solaar = {
     description = "Solaar - Logitech device manager";
     wantedBy = ["graphical-session.target"];
-    after = ["dbus.service"];
+    partOf = ["graphical-session.target"];
+    after = ["graphical-session.target"];
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${pkgs.solaar}/bin/solaar --window hide --battery-icons symbolic";
+      ExecStart = "${pkgs.solaar}/bin/solaar --window hide";
       Restart = "on-failure";
       RestartSec = "5";
     };
