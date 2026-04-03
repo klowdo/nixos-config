e34d019a39c6d815d39c073d84745b10d96ffc04
Felix Svensson <klowdo.fs@gmail.com>
2026-04-03 08:49:05 +0200

    fix(freecad): use unstable version

--- home/features/development/freecad.nix
+++ home/features/development/freecad.nix
@@ -11,7 +11,7 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = [
-      pkgs.freecad-wayland
+      pkgs.unstable.freecad-wayland
     ];
   };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -7,7 +7,7 @@
   strongswan = import ./strongswan.nix;
   dotnet-combined = import ./dotnet-combined.nix;
   claude-code = import ./claude-code.nix {inherit inputs;};
-  freecad = import ./freecad.nix;
+  # freecad = import ./freecad.nix;
   sesh = import ./sesh.nix;
 
   jetbrains-goland = import ./jetbrains-goland.nix;
