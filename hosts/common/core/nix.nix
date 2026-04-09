33554733fb5c512c6234d06c30416b2fcef5e36a
Felix Svensson <klowdo.fs@gmail.com>
2026-04-09 17:02:01 +0200

    chore(cachix): update public key

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -9,7 +9,7 @@
       "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "klowdo.cachix.org-1:FY4Cq5DPh6QYyU2R2PGgCNdXbh7u6H3kRRpmydZbdXc="
+      "klowdo.cachix.org-1:m9fa/ozb0xWATsMwp96qj/0pOIcaunR1Z3v8+nGPQhM="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
