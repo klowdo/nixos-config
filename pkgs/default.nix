f9c3b5746c8bf06a26718dab6b37e140dde7f0af
Felix Svensson <klowdo.fs@gmail.com>
2026-04-23 14:45:46 +0200

    chore(pkgs): remove claudia and zellij-ps
    Both broken: claudia has stale cargo vendor hash, zellij-ps depends on
    zellij which needs rustc 1.92 (nixos-25.11 has 1.91.1).

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,12 +7,12 @@
   # https://github.com/EmergentMind/nix-config/blob/14adb9899cb101d0599dc3d7aa88a70ba4cf0388/pkgs/default.nix
   # cd-gitroot = pkgs.callPackage ./cd-gitroot { };
   # zhooks = pkgs.callPackage ./zhooks { };
-  zellij-ps = pkgs.callPackage ./zellij-ps {};
+  # zellij-ps = pkgs.callPackage ./zellij-ps {};
   # ultimaker-cura = pkgs.callPackage ./cura {};
   appimage-tools = pkgs.callPackage ./appimage-tools {};
   auto-claude-appimage = pkgs.callPackage ./auto-claude-appimage {};
   bambustudio-appimage = pkgs.callPackage ./bambustudio-appimage {};
-  claudia = pkgs.callPackage ./claudia {};
+  # claudia = pkgs.callPackage ./claudia {};
   deezer-linux = pkgs.callPackage ./deezer-linux {};
   numara-calculator = pkgs.callPackage ./numara-calculator {};
   nss-docker-ng = pkgs.callPackage ./nss-docker-ng {};
