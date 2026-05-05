95702f0d63348094c18cffae27b99206b9242906
Felix Svensson <klowdo.fs@gmail.com>
2026-05-05 10:24:34 +0200

    fix(rustrover): use stable

--- home/features/development/tools/rustrover.nix
+++ home/features/development/tools/rustrover.nix
@@ -2,27 +2,21 @@
   config,
   lib,
   pkgs,
-  inputs,
   ...
 }:
 with lib; let
   cfg = config.features.development.tools.rustrover;
-  rustroverPkg = pkgs.unstable.jetbrains.rust-rover.override {forceWayland = true;};
+  rustroverPkg = pkgs.jetbrains.rust-rover.override {forceWayland = true;};
   rustroverVersion = concatStringsSep "." (take 2 (splitString "." rustroverPkg.version));
-  pluginList = with inputs.nix-jetbrains-plugins.plugins."${pkgs.stdenv.hostPlatform.system}".rust-rover."${rustroverVersion}"; [
-    IdeaVIM
-  ];
 in {
   options.features.development.tools.rustrover.enable = mkEnableOption "enable RustRover IDE";
   config = mkIf cfg.enable {
     home.packages = [
-      (pkgs.jetbrains.plugins.addPlugins rustroverPkg pluginList)
+      rustroverPkg
     ];
 
     home.file.".config/JetBrains/RustRover${rustroverVersion}/rustrover64.vmoptions".text = ''
       -Dawt.toolkit.name=WLToolkit
-      -Dsun.java2d.xrender.enabled=true
-      -Dsun.java2d.pmoffscreen=false
     '';
   };
 }
