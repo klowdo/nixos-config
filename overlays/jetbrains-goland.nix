c96c1f53d1b0a0e0228fd124edf9ff48488b29ce
Felix Svensson <klowdo.fs@gmail.com>
2026-06-16 09:12:12 +0200

    chore(goland): fix hash

--- overlays/jetbrains-goland.nix
+++ overlays/jetbrains-goland.nix
@@ -5,7 +5,7 @@ final: prev: {
     version = "2026.1.3";
     src = prev.fetchurl {
       url = "https://download.jetbrains.com/go/goland-${version}.tar.gz";
-      hash = "sha256-1FkDEaapyDbTPe4soOdyCHLkp/UT3rZ6siGyGyjmGJo=";
+      hash = "sha256-sR9BEAVFWMxqgdVNA81ZXCc5Gr0rRvsOkc3B/AORuDw=";
     };
     build_number = "261.25134.147";
   });
