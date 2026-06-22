1d43c16fc6ee864db82523c233b712253b6adca7
Felix Svensson <klowdo.fs@gmail.com>
2026-06-22 11:13:29 +0200

    feat(thyx): add SDDM greeter module with malachite preset

---
+++ hosts/common/optional/thyx.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib; let
+  cfg = config.extraServices.thyx;
+  thyxMalachite = inputs.thyx.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
+    postInstall =
+      (old.postInstall or "")
+      + ''
+        cp $out/share/sddm/themes/thyx/presets/malachite.conf \
+           $out/share/sddm/themes/thyx/theme.conf
+      '';
+  });
+in {
+  imports = [inputs.thyx.nixosModules.default];
+
+  options.extraServices.thyx.enable = mkEnableOption "thyx SDDM greeter";
+
+  config = mkIf cfg.enable {
+    services.displayManager = {
+      defaultSession = "hyprland-uwsm";
+      sddm.wayland.enable = true;
+      sddm.thyx = {
+        enable = true;
+        package = thyxMalachite;
+      };
+    };
+
+    security.pam.services.sddm.fprintAuth = true;
+  };
+}
