ff156dc36a656f61b71cb0240cb9cbb3e4e0da51
klowdo <6265447+klowdo@users.noreply.github.com>
2026-06-29 10:18:07 +0000

    chore(nix-update): bump solaar

--- overlays/solaar.nix
+++ overlays/solaar.nix
@@ -1,12 +1,12 @@
 # nix-update: solaar
 final: prev: {
   solaar = prev.solaar.overrideAttrs (oldAttrs: rec {
-    version = "1.1.19";
+    version = "1.1.20";
     src = prev.fetchFromGitHub {
       owner = "pwr-Solaar";
       repo = "Solaar";
       tag = version;
-      hash = "sha256-Z3rWGmFQmfJvsWiPgxQmfXMPHXAAiFneBaoSVIXnAV8=";
+      hash = "sha256-h/uiy0TtMicKch2cdXHur5DkvQun2sAw2HpFI7Qstqg=";
     };
     preConfigure = "";
   });
