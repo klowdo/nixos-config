baee6ccc6d1061d0581a34b5fbbf9c726d8125ce
klowdo <6265447+klowdo@users.noreply.github.com>
2026-07-27 09:24:04 +0000

    chore(nix-update): bump teams-for-linux

--- overlays/teams-for-linux.nix
+++ overlays/teams-for-linux.nix
@@ -1,23 +1,23 @@
 # nix-update: teams-for-linux
 final: prev: {
   teams-for-linux = prev.teams-for-linux.overrideAttrs (old: {
-    version = "2.13.0";
+    version = "2.14.0";
 
     src = prev.fetchFromGitHub {
       owner = "IsmaelMartinez";
       repo = "teams-for-linux";
-      rev = "v2.13.0";
-      hash = "sha256-30jt23bsJ1XE2gclRg06AM+mk1IrerNnkbWVDRfjqHo=";
+      rev = "v2.14.0";
+      hash = "sha256-yEw0QJx5uztxH3KmhePR40/LtCHWdzZ+WHNCXisG+7U=";
     };
 
     npmDeps = prev.fetchNpmDeps {
       src = prev.fetchFromGitHub {
         owner = "IsmaelMartinez";
         repo = "teams-for-linux";
-        rev = "v2.13.0";
-        hash = "sha256-30jt23bsJ1XE2gclRg06AM+mk1IrerNnkbWVDRfjqHo=";
+        rev = "v2.14.0";
+        hash = "sha256-yEw0QJx5uztxH3KmhePR40/LtCHWdzZ+WHNCXisG+7U=";
       };
-      hash = "sha256-pz2htdFmczmZJtcrpI/X0nUUF++x2vtcYZiTWjEYglo=";
+      hash = "sha256-rY2J6Q7dsY90TaVpcC2xWbMjIZ6YYF/Pl32pNL+pi9s=";
     };
   });
 }
