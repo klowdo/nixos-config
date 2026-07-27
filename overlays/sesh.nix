1c793dc92b3df82df24f069a17b4e6aaaca75c77
klowdo <6265447+klowdo@users.noreply.github.com>
2026-07-27 09:24:11 +0000

    chore(nix-update): bump sesh

--- overlays/sesh.nix
+++ overlays/sesh.nix
@@ -1,13 +1,13 @@
 # nix-update: sesh
 final: prev: {
   sesh = prev.sesh.overrideAttrs (oldAttrs: rec {
-    version = "2.27.0";
+    version = "2.28.0";
 
     src = prev.fetchFromGitHub {
       owner = "joshmedeski";
       repo = "sesh";
       rev = "v${version}";
-      hash = "sha256-py4Dok8nyyxqenaY5Clhik0W8vo3dsw2EhzUZxQtA38=";
+      hash = "sha256-e9OZ5EX3YVT2TMMh9cb4wNAbXezU0PWqQx7A9x9rxKo=";
     };
 
     ldflags = [
