199c80140a1c9da7fc3ae97b5b38fd8e0eb20fe3
klowdo <6265447+klowdo@users.noreply.github.com>
2026-08-27 16:59:21 +0000

    chore(nix-update): bump hyprmoncfg

--- pkgs/hyprmoncfg/default.nix
+++ pkgs/hyprmoncfg/default.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule rec {
   pname = "hyprmoncfg";
-  version = "1.15.1";
+  version = "1.16.1";
 
   src = fetchFromGitHub {
     owner = "crmne";
     repo = "hyprmoncfg";
     tag = "v${version}";
-    hash = "sha256-AmHe7a7LNV2HTgxJdqdY16X7mTnF/Fp8yr6/lAiTTUg=";
+    hash = "sha256-7wyghP1S7vauC2yJozaEg8ZfPgUNdgtaI4vq5XcTqGE=";
   };
 
   vendorHash = "sha256-gQbjvdKtO0hCXrs9RnWo1s0YeHf5W9t+8AgS2ELXlPo=";
